e7e07e0a6c40d4f95f64f1ea6a69bf47342e6863
danil <daniil.anef@gmail.com>
2022-10-10 18:14:41 +0400

    added comment

--- src/main/java/com/springApp/springSalaryApp/SpringSalaryAppApplication.java
+++ src/main/java/com/springApp/springSalaryApp/SpringSalaryAppApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringSalaryAppApplication {
-
+	//request example
+	//http://localhost:8080/calculacte/?countOfWeekendDays=14&averageSalary=70000
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSalaryAppApplication.class, args);
 	}
